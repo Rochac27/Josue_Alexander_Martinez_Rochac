a7d4f4ae17cc22dbffe9b8da45ed101397f1be19
Rochac27 <126750285+Rochac27@users.noreply.github.com>
2023-03-17 22:38:33 -0600

    Add files via upload

---
+++ Guia 8/Productosxd/basededatos/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using basededatos.Models;
+
+Console.WriteLine("Hello, World!");
+
+using (ProductosContext db = new ProductosContext())
+{
+    Productosxd productosxd = new Productosxd();
+
+    Console.WriteLine("Ingrese el nombre");
+    productosxd.Nombre = (Console.ReadLine());
+
+    Console.WriteLine("Ingrese una descripcion");
+    productosxd.Descripcion = (Console.ReadLine());
+
+    Console.WriteLine("Ingrese el precio");
+    productosxd.Precio = decimal.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese los stocks");
+    productosxd.Stock = int.Parse(Console.ReadLine());
+
+    db.Productosxds.Add(productosxd);
+    db.SaveChanges();
+
+
+    var ListProductosxd = db.Productosxds.ToList();
+
+    foreach (var usu in ListProductosxd)
+    {
+        Console.WriteLine(usu.Nombre);
+    }
+}
