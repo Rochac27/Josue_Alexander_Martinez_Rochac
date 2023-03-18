1592d66bf7f37f902ad39cfdb273fb7fe14c8041
Rochac27 <126750285+Rochac27@users.noreply.github.com>
2023-03-17 22:34:23 -0600

    Add files via upload

---
+++ Guia 8/basededatos/basededatos/Models/ProductosContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace basededatos.Models;
+
+public partial class ProductosContext : DbContext
+{
+    public ProductosContext()
+    {
+    }
+
+    public ProductosContext(DbContextOptions<ProductosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Productosxd> Productosxds { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server= DESKTOP-OD1595F\\SQLEXPRESS;Database=Productos;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Productosxd>(entity =>
+        {
+            entity.ToTable("PRODUCTOSXD");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(18, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
