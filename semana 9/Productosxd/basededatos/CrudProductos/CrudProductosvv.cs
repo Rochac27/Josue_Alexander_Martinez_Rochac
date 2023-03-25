b7391051831033b475a99a93e3a65f9492011168
Rochac27 <126750285+Rochac27@users.noreply.github.com>
2023-03-24 23:38:25 -0600

    Add files via upload

---
+++ semana 9/Productosxd/basededatos/CrudProductos/CrudProductosvv.cs
@@ -0,0 +1,30 @@
+﻿using basededatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basededatos.CrudProductos
+{
+    public class CrudProductosvv
+    {
+        public void InsertarProducto(Productosxd producto)
+        {
+            using (ProductosContext db = new ProductosContext())
+            {
+                db.Productosxds.Add(producto);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Productosxd> ObtenerProductos()
+        {
+            using (ProductosContext db = new ProductosContext())
+            {
+                return db.Productosxds.ToList();
+            }
+        }
+    }
+}
+
---
+++ semana 9/Productosxd/basededatos/Program.cs
@@ -0,0 +1,69 @@
+﻿using basededatos.CrudProductos;
+using basededatos.Models;
+using System;
+
+namespace basededatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrudProductosvv crudProductos = new CrudProductosvv();
+
+            while (true)
+            {
+                Console.WriteLine("Seleccione una opcion:");
+                Console.WriteLine("1. Insertar un nuevo producto");
+                Console.WriteLine("2. Listar todos los productos");
+                Console.WriteLine("3. Salir");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el nombre del producto");
+                        string nombre = Console.ReadLine();
+
+                        Console.WriteLine("Ingrese una descripcion del producto");
+                        string descripcion = Console.ReadLine();
+
+                        Console.WriteLine("Ingrese el precio del producto");
+                        decimal precio = decimal.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Ingrese los stocks del producto");
+                        int stock = int.Parse(Console.ReadLine());
+
+                        Productosxd producto = new Productosxd()
+                        {
+                            Nombre = nombre,
+                            Descripcion = descripcion,
+                            Precio = precio,
+                            Stock = stock
+                        };
+
+                        crudProductos.InsertarProducto(producto);
+                        Console.WriteLine("Producto agregado con éxito");
+                        break;
+
+                    case 2:
+                        var productos = crudProductos.ObtenerProductos();
+
+                        foreach (var p in productos)
+                        {
+                            Console.WriteLine($"Nombre: {p.Nombre}, Descripción: {p.Descripcion}, Precio: {p.Precio}, Stock: {p.Stock}");
+                        }
+                        break;
+
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción inválida, intente nuevamente.");
+                        break;
+                }
+            }
+        }
+    }
+}
