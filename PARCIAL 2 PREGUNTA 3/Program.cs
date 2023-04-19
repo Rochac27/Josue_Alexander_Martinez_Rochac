1edc9537d185820bf1385eca252fae66744778c1
Rochac27 <126750285+Rochac27@users.noreply.github.com>
2023-04-18 21:10:42 -0600

    Add files via upload

---
+++ PARCIAL 2 PREGUNTA 3/Program.cs
@@ -0,0 +1,13 @@
+﻿int numero1 = 10;
+string numero2 = "5";
+int resultado = 0;
+
+try
+{
+    resultado = numero1 + Convert.ToInt32(numero2);
+    Console.WriteLine("El resultado de la operación es: " + resultado);
+}
+catch
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
